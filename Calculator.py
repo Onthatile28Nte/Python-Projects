13d35fbf6137960659ea78b1a6d102d9488e9e65
Thabo <onthatilententesa@gmail.com>
2026-02-13 16:29:49 +0200

    Days 2 of pyhton calculator complete

--- Calculator.py
+++ Calculator.py
@@ -1,25 +1,39 @@
 print("==================== PYTHON CALCULATOR ===================")
 
-math_sign = input("Choose a sign (+, -, /, //, %, *, **): ")
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
 
 while True:
+    
+    math_sign = input("Choose a sign (+, -, /, //, %, *, **) or q to quit: ")
+    
+    if math_sign.lower() == "q":
+        break
+    
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    
+        
+    
     if math_sign == "+":
         print(f"Your answer is {num1 + num2}")
-    if math_sign == "-":
+        
+    elif math_sign == "-":
         print(f"Your answer is {num1 - num2}")
-    if math_sign == "/":
+        
+    elif math_sign == "/":
         if num2 == 0:
             print("Denominator cannot be zero.")
-    print(f"Your answer is {num1 / num2}")
-    if math_sign == "//":
+        else:
+            print(f"Your answer is {num1 / num2}")
+    elif math_sign == "//":
         print(f"Your answer is {num1 // num2}")
-    if math_sign == "%":
+    elif math_sign == "%":
         print(f"Your answer is {num1 % num2}")
-    if math_sign == "*":
-        print(f"Your answer is {num1 * num2}")
-    if math_sign == "**":
-        print(f"Your answer is {num1 ** num2}")
         
+    elif math_sign == "*":
+        print(f"Your answer is {num1 * num2}")
+        
+    elif math_sign == "**":
+        print(f"Your answer is {num1 ** num2}")
+    else:
+        print("Invalid Operator. Please try again.")
 
